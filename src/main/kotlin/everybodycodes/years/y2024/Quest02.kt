4e10c9852fa06c4184c912947a1243c3535be210
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-02-03 19:21:13 +0100

    2024: quest02.2

--- src/main/kotlin/everybodycodes/years/y2024/Quest02.kt
+++ src/main/kotlin/everybodycodes/years/y2024/Quest02.kt
@@ -2,6 +2,8 @@ package everybodycodes.years.y2024
 
 import everybodycodes.years.Quest
 import everybodycodes.years.readInput
+import kotlin.math.max
+import kotlin.math.min
 
 class Quest02 : Quest {
 
@@ -9,28 +11,54 @@ class Quest02 : Quest {
         val input = readInput("quest02.1", 2024)
         val words = input[0].substring("words:".length).split(",").map { it.trim() }
         val sentence = input[2]
+        return sentence.countWords(words)
+    }
 
-
-        return sentence.mapIndexed { index, c ->
-            words.map {
-                if (sentence.subword(index, it.length) == it) {
-                    1
-                } else {
-                    0
-                }
-            }.sum()
-        }.sum()
+    fun part2(): Any {
+        val input = readInput("quest02.2", 2024)
+        val words = input[0].substring("words:".length).split(",").map { it.trim() }
+        return input.drop(2)
+            .map {
+                val result = it.countWordChars(words)
+                return@map result
+            }
+            .sum()
     }
 }
 
 fun main() {
     val quest2 = Quest02()
     println(quest2.part1())
+    println(quest2.part2())
 }
 
-fun String.subword(start: Int, size: Int) : String? {
-    if (start in indices && start + size in indices) {
-        return this.substring(start, start + size)
+fun String.subword(start: Int, size: Int) : String {
+    val from = max(start, 0)
+    val to = min(start + size, length)
+    return this.substring(from, to)
+}
+
+fun String.countWords(words: List<String>) : Int {
+    return mapIndexed { index, c ->
+        words.map {
+            if (subword(index, it.length) == it) {
+                1
+            } else {
+                0
+            }
+        }.sum()
+    }.sum()
+}
+
+fun String.countWordChars(words: List<String>) : Int {
+    val fullWords = (words + words.map { it.reversed() }).toSet()
+    val chars = mutableSetOf<Int>()
+    forEachIndexed { index, _ ->
+        fullWords.forEach {
+            if (subword(index, it.length) == it) {
+                (index..<(index + it.length)).forEach { pos -> chars.add(pos) }
+            }
+        }
     }
-    return null
+    return chars.size
 }
